1e66c9b329959916244698580c55096a31afa220
achisum94 <34990703+achisum94@users.noreply.github.com>
2022-01-29 12:07:34 -0500

    Update settings.py

--- carzone/settings.py
+++ carzone/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'carzone_db',
         'USER': 'postgres',
-        'PASSWORD': 'Koolkid@666#',
+        'PASSWORD': '',
         'HOST': 'localhost'
     }
 }
